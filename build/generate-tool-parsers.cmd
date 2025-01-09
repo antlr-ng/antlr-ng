749394158f1b3d73c8b5588c096b74a34a3d4228
Mike Lischke <mike@lischke-online.de>
2025-01-09 14:27:08 +0100

    Setup right Java version

--- build/generate-tool-parsers.cmd
+++ build/generate-tool-parsers.cmd
@@ -14,8 +14,8 @@ exit 0
 
 echo Generating tool parsers...
 
-antlr4ng -Dlanguage=TypeScript -no-visitor -no-listener -Xexact-output-dir -o src\\generated src\\grammars\\ActionSplitter.g4
-antlr4ng -Dlanguage=TypeScript -no-visitor -no-listener -Xexact-output-dir -o src\\generated src\\grammars/ANTLRv4Lexer.g4
-antlr4ng -Dlanguage=TypeScript -no-visitor -no-listener -Xexact-output-dir -o src\\generated src\\grammars/ANTLRv4Parser.g4
+antlr4ng -Dlanguage=TypeScript -no-visitor -no-listener -Xexact-output-dir -o src\generated src\grammars\\ActionSplitter.g4
+antlr4ng -Dlanguage=TypeScript -no-visitor -no-listener -Xexact-output-dir -o src\generated src\grammars\ANTLRv4Lexer.g4
+antlr4ng -Dlanguage=TypeScript -no-visitor -no-listener -Xexact-output-dir -o src\generated src\grammars\ANTLRv4Parser.g4
 
 echo done
