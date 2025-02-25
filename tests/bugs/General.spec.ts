668eb15c58997e2a542883f2698726d9d576a2c2
Mike Lischke <mike@lischke-online.de>
2025-02-25 15:37:54 +0100

    Fixed parsing action blocks with predicate options
    Since actions are not predicates, it's unclear why they can have options, but one of the unit tests generates code which has that and it worked with the old ANTLR4, so I guess I have to support it too for now.

--- tests/bugs/General.spec.ts
+++ tests/bugs/General.spec.ts
@@ -9,7 +9,7 @@ import { fileURLToPath } from "node:url";
 import { describe, expect, it } from "vitest";
 
 import { DOTGenerator } from "../../src/tool/DOTGenerator.js";
-import { LexerGrammar } from "../../src/tool/index.js";
+import { Grammar, LexerGrammar } from "../../src/tool/index.js";
 
 describe("General", () => {
     it("Bug #33 Escaping issues with backslash in .dot file comparison", async () => {
@@ -42,4 +42,23 @@ describe("General", () => {
             `arrowhead = normal];`)).toBeGreaterThan(-1);
     });
 
+    it("Grammar with element options", () => {
+        // Element options are allowed after an action and after a predicate.
+        const grammarText = `grammar T;
+            s @after {global.antlrTestWriteLn!($ctx.toStringTree(null, this));} : e ;
+            e : a=e op=('*'|'/') b=e  {}{true}?
+            | a=e op=('+'|'-') b=e  {}<p=3>{true}?<fail='Message'>
+            | INT {}{}
+            | '(' x=e ')' {}{}
+            ;
+            INT : '0'..'9'+ ;
+            WS : (' '|'\\n') -> skip;`;
+
+        const grammar = new Grammar(grammarText);
+        grammar.tool.process(grammar, false);
+
+        const rule = grammar.getRule("e")!;
+        const startState = grammar.atn!.ruleToStartState[rule.index]!;
+        expect(startState).toBeDefined(); // We only need to see if the grammar parses without errors.
+    });
 });
